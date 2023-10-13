20167a6600bb933dd7dbf317e7dd26ec8dbbf29f
Leovilis <107759781+Leovilis@users.noreply.github.com>
2023-10-13 03:32:46 -0300

    actualizacion de codigo js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,73 @@
-console.log("ENTRO A JS");
-document.addEventListener('DOMContentLoaded', function() {
-    const confirmButton = document.getElementById('confirmButton');
-    const invitationDiv = document.getElementById('invitation');
-    const usernameInput = document.getElementById('username');
-
-    confirmButton.addEventListener('click', function() {
-        console.log("CONFIRMA");
-        localStorage.setItem('attendanceConfirmed', true);
-        invitationDiv.innerHTML = "<p>¡Gracias! Respondeme con un SI! <br> a mi Whatsapp</p>";
-
-        const name = usernameInput.value;
-        console.log("nombre: "+name);
-        if (name) {
-            localStorage.setItem('attendanceConfirmed', true);
-            localStorage.setItem('guestName', name);
-            usernameInput.disabled = 'true';
+ document.addEventListener('DOMContentLoaded', function() {
+        const confirmButton = document.getElementById('confirmButton');
+        const invitationDiv = document.getElementById('invitation');
+        const usernameInput = document.getElementById('username');
+    
+        confirmButton.addEventListener('click', function() {
+            const name = usernameInput.value;
+            if (name) {
+                localStorage.setItem('attendanceConfirmed', true);
+                localStorage.setItem('guestName', name);
+                usernameInput.disabled = true;
+                confirmButton.style.display = 'none';
+                invitationDiv.innerHTML = "<p>¡Gracias! Respondeme con un SI! <br> a mi Whatsapp</p>";
+            }
+        });
+    
+        const attendanceConfirmed = localStorage.getItem('attendanceConfirmed');
+        const guestName = localStorage.getItem('guestName');
+    
+        if (attendanceConfirmed && guestName) {
+            usernameInput.value = guestName;
+            usernameInput.disabled = true;
             confirmButton.style.display = 'none';
             invitationDiv.innerHTML = "<p>¡Gracias! Respondeme con un SI! <br> a mi Whatsapp</p>";
-            nameContainer.style.display = 'block';
-            nameContainer.innerHTML = `<p>${name}</p>`;
         }
-       
     });
 
-    const attendanceConfirmed = localStorage.getItem('attendanceConfirmed');
-    const guestName = localStorage.getItem('guestName');
-    console.log("guestname: "+guestName);
-    if (attendanceConfirmed && guestName) {
-        usernameInput.disabled = 'true';
-        confirmButton.style.display = 'none';
-        invitationDiv.innerHTML = "<p>¡Gracias! Respondeme con un SI! <br> a mi Whatsapp</p>";
-        nameContainer.style.display = 'block';
-        usernameInput.value = guestname;
-        // nameContainer.innerHTML = `<p>${guestName}</p>`;
-        console.log(guestName);
-    }
+
+
+
+
+
+
+// console.log("ENTRO A JS");
+// document.addEventListener('DOMContentLoaded', function() {
+//     const confirmButton = document.getElementById('confirmButton');
+//     const invitationDiv = document.getElementById('invitation');
+//     const usernameInput = document.getElementById('username');
+
+//     confirmButton.addEventListener('click', function() {
+//         console.log("CONFIRMA");
+//         localStorage.setItem('attendanceConfirmed', true);
+//         invitationDiv.innerHTML = "<p>¡Gracias! Respondeme con un SI! <br> a mi Whatsapp</p>";
+
+//         const name = usernameInput.value;
+//         console.log("nombre: "+name);
+//         if (name) {
+//             localStorage.setItem('attendanceConfirmed', true);
+//             localStorage.setItem('guestName', name);
+//             usernameInput.disabled = 'true';
+//             confirmButton.style.display = 'none';
+//             invitationDiv.innerHTML = "<p>¡Gracias! Respondeme con un SI! <br> a mi Whatsapp</p>";
+//             nameContainer.style.display = 'block';
+//             nameContainer.innerHTML = `<p>${name}</p>`;
+//         }
+       
+//     });
+
+//     const attendanceConfirmed = localStorage.getItem('attendanceConfirmed');
+//     const guestName = localStorage.getItem('guestName');
+//     console.log("guestname: "+guestName);
+//     if (attendanceConfirmed && guestName) {
+//         usernameInput.disabled = 'true';
+//         confirmButton.style.display = 'none';
+//         invitationDiv.innerHTML = "<p>¡Gracias! Respondeme con un SI! <br> a mi Whatsapp</p>";
+//         nameContainer.style.display = 'block';
+//         usernameInput.value = guestname;
+//         // nameContainer.innerHTML = `<p>${guestName}</p>`;
+//         console.log(guestName);
+//     }
 
     // createUserButton.addEventListener('click', function() {
     //     console.log("CREA USUARIO");
@@ -59,4 +92,4 @@ document.addEventListener('DOMContentLoaded', function() {
     // if (attendanceConfirmed && !votedUser) {
     //     votingDiv.style.display = 'block';
     // }
-});
+// });
